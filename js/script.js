2aad7354fd073eb5ed0a342e55d3b63bdf7174db
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-04-05 19:43:08 +0300

    Add win check

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-const cellElements = document.querySelectorAll('[data-cell]');
+const cellElements = document.querySelectorAll('.cell');
 const board = document.querySelector('.board');
 const xClass = 'x';
 const circleClass = 'circle';
+const winningMessageEl = document.querySelector('.winning-message');
+const winningMessageText = document.querySelector('.winning-message__text');
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 let circleTurn;
 
 const handleClick = function (e) {
@@ -23,7 +35,29 @@ const handleClick = function (e) {
     if (!circleTurn) board.classList.add(xClass);
   };
 
+  const checkWin = function (currentClass) {
+    return winningCombinations.some(combination => {
+      return combination.every(index => {
+        return cellElements[index].classList.contains(currentClass);
+      });
+    });
+  };
+
   placeMark(cell, currentClass);
+  // checkWin();
+
+  const endGame = function (draw) {
+    if (draw) {
+    } else {
+      winningMessageText.textContent = `${circleTurn ? "O's" : " X's"} Wins!`;
+    }
+    winningMessageEl.classList.add('show');
+  };
+
+  if (checkWin(currentClass)) {
+    endGame();
+  }
+
   swapTurns();
   setBoardHoverClass();
 };
